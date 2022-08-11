22c7fe9780232af773feacd886351de271671c84
resqiar <resqiageng@gmail.com>
2022-08-11 21:07:55 +0700

    chore: bind host to 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   app.enableCors();
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 5000, '0.0.0.0');
 }
 bootstrap();
