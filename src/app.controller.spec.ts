3b890b649fe1793064c47d20461be15984e2ab54
resqiar <resqiageng@gmail.com>
2022-11-21 22:33:05 +0700

    test(App): set test suite for root controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ImageKitSignature } from './types/image-kit';
 
 describe('AppController', () => {
   let appController: AppController;
 
+  const mockAppService = {
+    getHello: jest.fn(() => {
+      return 'Hello World!';
+    }),
+    getSignatureIK: jest.fn(() => {
+      return {
+        token: 'a11ca8b6-8f9d-41b5-a617-XXXXXXXXX',
+        expire: 1669044135,
+        signature: '9395c59b667cec60ded1ab5359e59883XXXXXXX',
+      };
+    }),
+  };
+
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        {
+          provide: AppService,
+          useValue: mockAppService,
+        },
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
@@ -17,6 +36,17 @@ describe('AppController', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
+    });
+  });
+
+  describe('Auth ImageKit', () => {
+    it('Should return a token from ImageKit', () => {
+      const result = {
+        token: expect.any(String),
+        expire: expect.any(Number),
+        signature: expect.any(String),
+      };
+      expect(appController.getSignatureIK()).toEqual(result);
     });
   });
 });
