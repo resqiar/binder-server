7e3071838c43afcda4f0222513c5366cb63b35c1
resqiar <resqiageng@gmail.com>
2022-11-24 06:12:05 +0700

    test: complete unit test for code-runner extension

---
+++ src/units/controllers/code-runner.controller.spec.ts
@@ -0,0 +1,55 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CodeRunnerService } from '../../services/code-runner.service';
+import {
+  CodeRunnerController,
+  CodeRunnerOutput,
+} from '../../controllers/code-runner.controller';
+import { CodeRunnerInput } from '../../dtos/code-runner.input';
+
+describe('CodeRunnerController', () => {
+  let codeRunnerController: CodeRunnerController;
+
+  const mockCodeRunnerService = {
+    callJdoodle: jest.fn((_: string) => {
+      const result: CodeRunnerOutput = {
+        cpuTime: '0.08',
+        memory: '200',
+        statusCode: 200,
+        output: 'Hello World!',
+      };
+      return result;
+    }),
+  };
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [CodeRunnerController],
+      providers: [
+        {
+          provide: CodeRunnerService,
+          useValue: mockCodeRunnerService,
+        },
+      ],
+    }).compile();
+
+    codeRunnerController = app.get<CodeRunnerController>(CodeRunnerController);
+  });
+
+  it('should be defined', () => {
+    expect(codeRunnerController).toBeDefined();
+  });
+
+  describe('Run Code Endpoint', () => {
+    const input = new CodeRunnerInput();
+
+    it('should be call the api and return CodeRunnerOutput', async () => {
+      const res = expect.objectContaining({
+        cpuTime: expect.any(String),
+        memory: expect.any(String),
+        statusCode: 200,
+        output: expect.any(String),
+      });
+      await expect(codeRunnerController.runCode(input)).resolves.toEqual(res);
+    });
+  });
+});
