b547feb77c7b9ddd0300c6c0ea159edc1366319c
resqiar <resqiageng@gmail.com>
2022-11-24 06:12:45 +0700

    refactor(CodeRunner): change to relative import due to jest incompatibility

--- src/controllers/code-runner.controller.ts
+++ src/controllers/code-runner.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
-import { CodeRunnerInput } from 'src/dtos/code-runner.input';
-import { CodeRunnerService } from 'src/services/code-runner.service';
+import { CodeRunnerInput } from '../dtos/code-runner.input';
+import { CodeRunnerService } from '../services/code-runner.service';
 
 export interface CodeRunnerOutput {
   output: string;
